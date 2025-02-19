2dd614b07cd06f7cc76d546d3cbf84f3fecc503d
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-02-19 20:00:47 +0530

    Update App.js to enhance UI with a welcome message and new styles

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to my first app</Text>
     </View>
   );
 }
@@ -13,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color:"red",
+    fontSize:40,
+    backgroundColor:"black"
+  }
 });
